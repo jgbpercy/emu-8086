8d92b94de05cf7d76e946306e0ffbaf54acd1dec
Joe Percy <jgbpercy@gmail.com>
2023-03-06 00:51:33 +0000

    Add download button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,10 +22,21 @@ export class AppComponent {
       const reader = new FileReader();
       reader.addEventListener('load', (loadEvt) => {
         console.log(loadEvt.target?.result);
-        console.log(typeof loadEvt.target?.result);
 
         if (loadEvt.target?.result instanceof ArrayBuffer) {
-          const decodedInstructions = decodeInstructions(new Uint8Array(loadEvt.target.result));
+          const instructionBytes = new Uint8Array(loadEvt.target.result);
+          let index = 0;
+          for (const byte of instructionBytes) {
+            console.log(
+              `${' '.repeat(4 - index.toString().length)}${index}: ${'0'.repeat(
+                8 - byte.toString(2).length,
+              )}${byte.toString(2)}`,
+            );
+
+            index++;
+          }
+
+          const decodedInstructions = decodeInstructions(instructionBytes);
 
           console.log(decodedInstructions);
           this.instructionString = printDecodedInstructions(decodedInstructions);
@@ -36,5 +47,22 @@ export class AppComponent {
         reader.readAsArrayBuffer(evt.target.files[0]);
       }
     }
+  }
+
+  download(): void {
+    const blob = new Blob([this.instructionString]);
+
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'my-assembly.asm';
+
+    document.body.appendChild(a);
+
+    a.click();
+
+    window.URL.revokeObjectURL(url);
   }
 }
